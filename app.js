8b33ae9145b26e80c12e19269549ba1da4844297
Ajith K.R <46483037+ajithkr12@users.noreply.github.com>
2024-12-30 14:54:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get("/", function (req, res) {
 app.get("/new", function (req, res) {
   res.send("New also worked");
 });
-
+console.log("");
 app.listen(process.env.PORT || 5000);
 module.exports = app;
